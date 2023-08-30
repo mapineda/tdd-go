ee16bcf3c4a25071e58c3a7e0ddd7cf47aeaac49
Marco Pineda <mapineda1222@gmail.com>
2023-08-30 15:06:00 -0500

    refactor with switch statements

--- hello.go
+++ hello.go
@@ -2,14 +2,33 @@ package main
 
 import "fmt"
 
+const spanish = "Spanish"
+const english = "English"
+const french = "French"
+
 const latinHelloPrefix = "Salve, "
-func Hello(name string) string  {
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
+
+func Greet(name string, language string) string  {
 	if name == "" {
 		name = "World"
 	}
-	return latinHelloPrefix + name
+
+	prefix := latinHelloPrefix
+
+	switch language {
+		case spanish:
+			prefix = spanishHelloPrefix
+		case english:
+			prefix = "Hello, "
+		case french:
+			prefix = "Bonjour, "
+	}
+
+	return prefix + name
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Greet("world", "english"))
 }
--- hello_test.go
+++ hello_test.go
@@ -4,16 +4,32 @@ import "testing"
 
 func TestHello(t *testing.T ) {
 	t.Run("say Hello to people", func(t *testing.T) {
-		result := Hello("Chris")
+		result := Greet("Chris", "")
 		expected := "Salve, Chris"
-		
 		assertCorrectMessage(t, result, expected)
 	})
 
 	t.Run("empty string defaults to 'world'", func(t *testing.T) {
-		result := Hello("")
+		result := Greet("", "")
 		expected := "Salve, World"
+		assertCorrectMessage(t, result, expected)
+	})
 
+	t.Run("in Spanish", func(t *testing.T) {
+		result := Greet("Elodie", "Spanish")
+		expected := "Hola, Elodie"
+		assertCorrectMessage(t, result, expected)
+	})
+
+	t.Run("In English", func(t *testing.T) {
+		result := Greet("Chris", "English")
+		expected := "Hello, Chris"
+		assertCorrectMessage(t, result, expected)
+	})
+
+	t.Run("In French", func(t *testing.T) {
+		result := Greet("Chris", "French")
+		expected := "Bonjour, Chris"
 		assertCorrectMessage(t, result, expected)
 	})
 }
